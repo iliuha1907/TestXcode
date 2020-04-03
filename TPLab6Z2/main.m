0ccd1225e08d2419d15b10b016ad90578e78535f
Ilya <iaguzey5@gmail.com>
2020-04-03 04:18:31 -0700

    Initial Commit

---
+++ TPLab6Z2/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  TPLab6Z2
+//
+//  Created by Ilya Huzei on 03.04.2020.
+//  Copyright © 2020 Ilya Huzei. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
