93012a8e693e6fa59699286393dd25e41c6a8a86
Ilya <iaguzey5@gmail.com>
2020-04-03 10:32:29 -0700

    Write code

--- TPLab6Task5/main.swift
+++ TPLab6Task5/main.swift
@@ -8,5 +8,28 @@
 
 import Foundation
 
-print("Hello, World!")
+var table = [Int](repeating: 0, count: 256)
+var tableCount = [Character: Int]()
+
+func processString(text: String){
+    let offset = 128
+    for code in text.utf8 {
+        let ind = Int(code) + offset
+        table[ind] += 1
+        tableCount.updateValue(table[ind], forKey: Character(UnicodeScalar(code)))
+    }
+    print(tableCount)
+    let sortedDict = tableCount.sorted { $0.1 < $1.1 }
+    for (index, keyValue) in sortedDict {
+        print("Letter:  \(index) - Count:  \(keyValue)")
+    }
+}
+
+func main(){
+    print("Hello, World!")
+    processString(text: "abca abcd")
+    
+}
+
+main()
 
