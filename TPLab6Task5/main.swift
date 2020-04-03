26efb24dba6ebd0a373a9876dfaa95287aab23cd
Ilya <iaguzey5@gmail.com>
2020-04-03 07:23:27 -0700

    Initial Commit

---
+++ TPLab6Task5/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  TPLab6Task5
+//
+//  Created by Ilya Huzei on 03.04.2020.
+//  Copyright © 2020 Ilya Huzei. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
